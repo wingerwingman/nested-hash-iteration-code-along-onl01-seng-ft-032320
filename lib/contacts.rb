0c89544a3a01eee06a74bbe6534e60a321c50d18
Justin Winger <justinwinger4@gmail.com>
2020-03-27 16:14:18 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,6 +16,9 @@ require 'pry'
   
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-    binding.pry
+    if person == "Fredy Mercury"
+      contact_details_hash.each do |attribue, data|
+    # binding.pry
+  end
   end
 end
